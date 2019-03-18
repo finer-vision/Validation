9238bf00bd26e8f52544c80d5ed735e2339fd410
James Craig <jamescraig@finervision.com>
2019-03-18 16:33:12 +0000

    Version 3.0.17 release

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Rule from "./Rule";
 import Error from "./Error";
 
-export default class Validation {
+class Validation {
     constructor() {
         this.errors = [];
         this.fields = [];
@@ -129,3 +129,7 @@ export default class Validation {
         return this;
     }
 }
+
+export {Validation};
+
+export default Validation;
